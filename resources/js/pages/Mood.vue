f4099dae643c747f6ca08060250b22475cd6a41c
lieuwebakker <3v8z4q@gmail.com>
2025-01-04 19:55:39 +0100

    Update Mood.vue

--- resources/js/pages/Mood.vue
+++ resources/js/pages/Mood.vue
@@ -38,30 +38,52 @@ const _subHeader=ref(true);
 const _sideBar = ref(false);
 
 
+
+
+
+
+
+const openSide = ref(false)
+const _toggleProject = async () => { mySideRight.value.open = !mySideRight.value.open; }
+const whatever = async () => { console.log('whatever')}
+
+const navigation = [
+//  { name: 'Dashboard', icon: TableCellsIcon, href: 'whatever', current: true },
+//  { name: 'Team', icon: TableCellsIcon, href: 'whatever', current: false },
+//  { name: 'Projects', icon: TableCellsIcon, href: 'whatever', current: false },
+//  { name: 'Calendar', icon: TableCellsIcon, href: '#', current: false },
+//  { name: 'Documents', icon: TableCellsIcon, href: '#', current: false },
+  { name: 'Reports', icon: TableCellsIcon, href: whatever, current: false },
+  { name: 'Project', icon: ClipboardDocumentCheckIcon, href: _toggleProject, current: false },
+];
+
+
+
+
+
+
+
+
+
+
+
+// Theme Buttons
+const _setWillow = () => { _set.dark=false; myChild.value.changeTheme('willow'); }
+const _setMaterial = () => { _set.dark=false; myChild.value.changeTheme('material');}
+const _setWillowDark = () => { _set.dark=true; myChild.value.changeTheme('willow-dark');}
+
 // css
 const _title = "text-indigo-600 dark:text-indigo-300";
 const _shadow = "shadow-lg shadow-gray-300 dark:shadow-slate-600";
-const _button ="rounded px-2 py-1 text-xs font-semibold text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-indigo-300 dark:ring-gray-600 ";
+//const _button ="rounded px-2 py-1 text-xs font-semibold text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-indigo-300 dark:ring-gray-600 ";
 const _hover = "hover:bg-indigo-400 dark:hover:bg-indigo-600";
 const _bg = "bg-white dark:bg-black";
 const _bgSelected = "bg-indigo-200 dark:bg-indigo-800";
 const _hoverAdd = "hover:bg-green-400 dark:hover:bg-green-600";
 const _hoverDelete = "hover:bg-red-400 dark:hover:bg-red-600";
-
-
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Documents', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-]
-
-
 const _indigo = " text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-yellow-400 ";
+const _button = "mt-2.5 mx-1 rounded px-2 py-1 text-xs ring-1 ring-inset ring-gray-300 dark:ring-gray-600 hover:ring-indigo-400 dark:hover:ring-indigo-600";
 
-const openSide = ref(false)
 
 </script>
 <template>
@@ -72,6 +94,9 @@ const openSide = ref(false)
             <HeaderSection v-if="_header" :set="_set" :contract="_contract"/>
             <SubHeaderSection v-if="_subHeader" :set="_set"/>
             <SideRightSection ref="mySideRight" :set="_set" />
+
+            <!-- buttons subHeader -->
+            <!--             
             <div v-if="true" class="absolute space-x-2 z-40" :class="[_header ? 'top-16' : 'top-1', _subHeader ? 'left-32' : 'left-80']" >
               <button v-if="_subHeader" @click="myChild._save" :disabled="_set.project.id==0" type="button" class="rounded bg-white dark:bg-slate-950 px-4 py-1 text-xs font-semibold text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 hover:bg-green-400 dark:hover:ring-green-600" :class="_set.project.id==0 ? 'opacity-35' : '' ">Save</button>
               <button v-if="_subHeader" @click="myChild._load" :disabled="_set.project.id==0" type="button" class="rounded bg-white dark:bg-slate-950 px-4 py-1 text-xs font-semibold text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 hover:bg-green-400 dark:hover:ring-green-600" :class="_set.project.id==0 ? 'opacity-35' : '' ">Load</button>
@@ -83,6 +108,9 @@ const openSide = ref(false)
               <button v-if="_subHeader"  @click="_header=!_header" type="button" class="rounded bg-white dark:bg-slate-950 px-2 py-1 text-xs font-semibold text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 hover:bg-indigo-400 dark:hover:ring-indigo-600">Header</button>
               <button @click="_subHeader=!_subHeader" type="button" class="rounded bg-white dark:bg-slate-950 px-1 py-1 text-xs font-semibold text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 hover:bg-indigo-400 dark:hover:ring-indigo-600">subHeader</button>
             </div>
+            -->
+
+
         </template>
 
         <template #default>
@@ -106,6 +134,7 @@ const openSide = ref(false)
                         <wrench-icon @click="mySideRight.open = !mySideRight.open" class="w-10 px-2.5 pt-3 mx-2 " :class="_indigo" />
                         <question-mark-circle-icon class="w-10 px-2.5 pt-3 mx-2" :class="_indigo" />
                         <li v-for="item in navigation" :key="item.name">
+                            <component :is="item.icon" @click="item.href" class="w-10 px-2.5 pt-3 mx-2" :class="_indigo" />
                         </li>
                     </ul>
                   </nav>
@@ -115,9 +144,22 @@ const openSide = ref(false)
                     <bars3-icon @click="_sideBar = !_sideBar" class="w-6 px-1 mt-3" :class="_indigo" />
                 </div>
 
-                <div id="whatever" class="w-full ... min-h-4 max-w-9xl dark:bg-black">
-                    <div class="" id="toolbar"></div>
-                    <PanaderoMood ref="myChild" :contract="_contract" :set="_set" :db="_db" :pulse="pulse"/>
+                <div id="whatever" class="w-full ... min-h-4 max-w-9xl bg-white">
+                    <div class="grid grid-cols-2">     
+                        
+                        <div class="pl-2" :class="_set.dark ? 'wx-willow-dark-theme' : 'wx-willow-theme'">
+                            
+                            <button @click="_setWillow" type="button" :class="_button">Light</button>
+                            <button @click="_setWillowDark" type="button" :class="_button">Dark</button>
+                            <button @click="_setMaterial" type="button" :class="_button">Material</button>
+                            <button @click="myChild._save" type="button" :class="_button">Save</button>
+                            <button @click="myChild._load" type="button" :class="_button">Load</button>
+                        
+                        </div>      
+
+                        <div class="" id="toolbar"></div>
+                    </div>
+                        <PanaderoMood ref="myChild" :contract="_contract" :set="_set" :db="_db" :pulse="pulse"/>
                 </div>
             </div>
         </template>
