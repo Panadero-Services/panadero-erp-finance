ced8e50ce06b88d4b1710b4607df344291095a0a
lieuwebakker <3v8z4q@gmail.com>
2025-04-25 10:11:10 +0200

    Update RightSectionLayout.vue

--- resources/js/sections/layouts/RightSectionLayout.vue
+++ resources/js/sections/layouts/RightSectionLayout.vue
@@ -20,7 +20,7 @@ const _theme = computed(() => {
    <TransitionRoot as="template" :show="open">
       <Dialog class="relative z-10" @close="open=false">
          <div class="fixed inset-0 overflow-hidden">
-            <div class="absolute inset-0 overflow-hidden">
+            <div class="absolute inset-0 overflow-hidden" :class="_theme">
                <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16 ">
                   <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
                      <DialogPanel class="pointer-events-auto w-screen max-w-md">
