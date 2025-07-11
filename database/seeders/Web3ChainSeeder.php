26f87693e1920a27052ecb69a955e5dcd020d600
lieuwebakker <3v8z4q@gmail.com>
2025-07-11 16:37:20 +0200

    Update Web3ChainSeeder.php

--- database/seeders/Web3ChainSeeder.php
+++ database/seeders/Web3ChainSeeder.php
@@ -11,57 +11,39 @@ class Web3ChainSeeder extends Seeder
     {
         $chains = [
             [
-                'id' => 1,
                 'title' => 'BSC',
                 'chain_nr' => 56,
                 'icon' => null,
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ],
             [
-                'id' => 2,
                 'title' => 'BSC Testnet',
                 'chain_nr' => 97,
                 'icon' => null,
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ],
             [
-                'id' => 3,
                 'title' => 'Goerli',
                 'chain_nr' => 5,
                 'icon' => null,
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ],
             [
-                'id' => 4,
                 'title' => 'Polygon',
                 'chain_nr' => 137,
                 'icon' => null,
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ],
             [
-                'id' => 5,
                 'title' => 'Polygon Testnet',
                 'chain_nr' => 80001,
                 'icon' => null,
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ],
             [
-                'id' => 6,
                 'title' => 'MultiVAC',
                 'chain_nr' => 62621,
                 'icon' => null,
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ]
         ];
