0863d26e0c24e15eb7104562806650a0ae4358bc
lieuwebakker <3v8z4q@gmail.com>
2025-07-11 16:38:18 +0200

    Update Web3ProjectSeeder.php

--- database/seeders/Web3ProjectSeeder.php
+++ database/seeders/Web3ProjectSeeder.php
@@ -11,30 +11,21 @@ class Web3ProjectSeeder extends Seeder
     {
         $projects = [
             [
-                'id' => 1,
                 'title' => 'atpad',
                 'content' => '',
                 'color' => 'yellow-500',
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ],
             [
-                'id' => 2,
                 'title' => 'swappy',
                 'content' => '',
                 'color' => 'gray-200',
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ],
             [
-                'id' => 3,
                 'title' => 'self',
                 'content' => '',
                 'color' => 'indigo-400',
-                'created_at' => null,
-                'updated_at' => null,
                 'is_active' => true
             ]
         ];
